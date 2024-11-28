a7cd49e1c41b549aa03cac0a5b0308001cf54e2d
30245742 <30245742@blackpool.ac.uk>
2024-11-28 15:21:14 +0000

    Version 2
    Introduced a subclass PerishableProduct inheriting from Product.
    Added private methods and properties to demonstrate encapsulation.

--- LightLogisticsOOP/LightLogisticsOOP/LightLogisticsOOP.py
+++ LightLogisticsOOP/LightLogisticsOOP/LightLogisticsOOP.py
@@ -1,20 +1,27 @@
 class Product:
     def __init__(self, product_id, name, quantity, price):
-        self.product_id = product_id  # Encapsulation
+        self.__product_id = product_id  # Private attribute
         self.name = name
         self.quantity = quantity
         self.price = price
 
+    def get_product_id(self):  # Encapsulation via getter
+        return self.__product_id
+
+class PerishableProduct(Product):  # Inheritance
+    def __init__(self, product_id, name, quantity, price, expiration_date):
+        super().__init__(product_id, name, quantity, price)
+        self.expiration_date = expiration_date
+
 class InventoryManager:
     def __init__(self):
-        self.products = {}  # Using a dictionary to store products
+        self.products = {}
 
     def add_product(self, product):
-        self.products[product.product_id] = product
+        self.products[product.get_product_id()] = product
 
 # Usage Example
 if __name__ == "__main__":
     inv_manager = InventoryManager()
-    prod = Product(1, "Widget", 100, 2.99)
-    inv_manager.add_product(prod)
-
+    perishable = PerishableProduct(2, "Milk", 50, 1.99, "2023-12-31")
+    inv_manager.add_product(perishable)
